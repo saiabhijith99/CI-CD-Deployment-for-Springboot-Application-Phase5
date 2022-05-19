55472a23d696b6212abb5b5ac60deec02a8e08ec
saiabhijith99 <saiabhijith99@gmail.com>
2022-05-19 12:07:29 +0530

    CI/CD Deployment for Springboot Application

---
+++ Spring-Boot-Web-App/src/main/java/com/phase5/springboot/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.phase5.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	 @RequestMapping("/")
+	  public String index() {
+	    return "index.html";
+	  }
+}
